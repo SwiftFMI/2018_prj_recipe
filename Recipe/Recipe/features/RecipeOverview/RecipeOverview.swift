12e9c27e40fe0efdb1e2bafa20a202168437eaed
Nasko-Kurtakov <nasko.kurtakov@gmail.com>
2019-02-17 16:46:42 +0200

    recipe view starters

--- Recipe/Recipe/features/RecipeOverview/RecipeOverview.swift
+++ Recipe/Recipe/features/RecipeOverview/RecipeOverview.swift
@@ -10,7 +10,8 @@ import Foundation;
 import UIKit;
 
 class RecipeOverviewController: UIViewController {
-    @IBOutlet weak var recipeName: UILabel!
+	@IBOutlet weak var scrollView: UIScrollView!
+	@IBOutlet weak var recipeName: UILabel!
     @IBOutlet weak var recipeAuthor: UILabel!
     @IBOutlet weak var recipeDescription: UILabel!
     @IBOutlet weak var recipeIngredients: UILabel!
@@ -18,14 +19,21 @@ class RecipeOverviewController: UIViewController {
     @IBOutlet weak var recipeTimeToPrepare: UILabel!
     @IBOutlet weak var recipeTimeToCook: UILabel!
     var currentRecipe: ShortRecipe = ShortRecipe(name: "Lorem", description: "Impsum", photo: #imageLiteral(resourceName: "DefaulRecipeImage"))!
-    
-    override func viewDidLoad() {
-        self.recipeName.text = "Some Recipe Name"
-        self.recipeDescription.text = currentRecipe.description
-        self.recipeTimeToPrepare.text = currentRecipe.timeToPrepare
-        self.recipeTimeToCook.text = currentRecipe.timeToCook
-        self.recipeIngredients.text = currentRecipe.ingredients
-        self.recipeAuthor.text = "Mihail Kirilov"
-        super.viewDidLoad();
+	
+	
+	override func viewDidAppear(_ animated: Bool) {
+		scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height+300)
+	}
+	override func viewDidLoad() {
+//        self.recipeName.text = "Some Recipe Name"
+//        self.recipeDescription.text = currentRecipe.description
+//        self.recipeTimeToPrepare.text = currentRecipe.timeToPrepare
+//        self.recipeTimeToCook.text = currentRecipe.timeToCook
+//        self.recipeIngredients.text = currentRecipe.ingredients
+//        self.recipeAuthor.text = "Mihail Kirilov"
+//        super.viewDidLoad();
+	}
+	override func viewWillLayoutSubviews(){
+		super.viewWillLayoutSubviews();
 	}
 }
