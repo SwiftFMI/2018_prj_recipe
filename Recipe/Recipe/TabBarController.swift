c6ae32fbfd2d85fd3e834fd6b83906cd63f685a9
Nasko-Kurtakov <nasko.kurtakov@gmail.com>
2019-02-18 22:03:27 +0200

    Tabbar update to controll the other views.

--- Recipe/Recipe/TabBarController.swift
+++ Recipe/Recipe/TabBarController.swift
@@ -11,16 +11,20 @@ class TabBarController: RAMAnimatedTabBarController {
 	override func viewDidLoad() {
 		super.viewDidLoad();
 		
-		let exploreScene = ExploreController.instantiate(fromAppStoryboard: .Explore);
-		let favouritesScene = FavouritesController.instantiate(fromAppStoryboard: .Favourites);
-		let shoppingListScene = ShoppingListController.instantiate(fromAppStoryboard: .ShoppingList);
-		let createRecipeScene = CreateRecipeController.instantiate(fromAppStoryboard: .CreateRecipe);
-		let abaoutScene = AboutPageController.instantiate(fromAppStoryboard: .AboutPage);
 		
-		self.viewControllers = [exploreScene,favouritesScene,shoppingListScene,createRecipeScene,abaoutScene];
-		
-		
-		
+		ref.child("allRecipeIDs").observe(.value) { (snapshot) in
+			
+			cachedRecipeIDS[snapshot.key ?? ""] = ""
+			
+			let exploreScene = ExploreController.instantiate(fromAppStoryboard: .Explore);
+			let favouritesScene = FavouritesController.instantiate(fromAppStoryboard: .Favourites);
+			let shoppingListScene = ShoppingListController.instantiate(fromAppStoryboard: .ShoppingList);
+			let createRecipeScene = CreateRecipeController.instantiate(fromAppStoryboard: .CreateRecipe);
+			let abaoutScene = AboutPageController.instantiate(fromAppStoryboard: .AboutPage);
+			
+			self.viewControllers = [exploreScene,favouritesScene,shoppingListScene,createRecipeScene,abaoutScene];
+		}
+		print("asd");
 	}
 	
 }
